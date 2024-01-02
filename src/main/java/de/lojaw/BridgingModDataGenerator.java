24743d1653b3213a350ebc58489b1fa6b0ecbea6
Lojaw <j.p.schmitz1@web.de>
2024-01-02 21:12:47 +0100

    Initial commit

---
+++ src/main/java/de/lojaw/BridgingModDataGenerator.java
@@ -0,0 +1,11 @@
+package de.lojaw;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BridgingModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
